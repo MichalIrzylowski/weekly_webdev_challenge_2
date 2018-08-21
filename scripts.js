90394fafe0ffdf1d34a4482c5138fe46081c1177
MichalIrzylowski <michal.irzylowski@gmail.com>
2018-08-21 22:46:55 +0200

    Updates

--- scripts.js
+++ scripts.js
@@ -8,10 +8,28 @@ window.addEventListener("scroll", function() {
   } else if (this.window.scrollY <= 100) {
     menu.classList.remove("smaller");
   }
+  if (window.scrollY < 1000) {
+    itemRadio[0].checked = true;
+  }
+  if (window.scrollY >= 1000 && window.scrollY < 1700) {
+    itemRadio[1].checked = true;
+  }
+  if (window.scrollY >= 1700 && window.scrollY < 2490) {
+    itemRadio[2].checked = true;
+  }
+  if (window.scrollY >= 2490 && window.scrollY < 4026) {
+    itemRadio[3].checked = true;
+  }
+  if (window.scrollY >= 4526 && window.scrollY < 5390) {
+    itemRadio[4].checked = true;
+  }
+  if (window.scrollY >= 5390) {
+    itemRadio[5].checked = true;
+  }
 });
 
-for (let i = 0; i < menuItems.length; i++) {
-  menuItems[i].addEventListener("click", function() {
-    itemRadio[i].checked = true;
-  });
-}
+// for (let i = 0; i < menuItems.length; i++) {
+//   menuItems[i].addEventListener("click", function() {
+//     itemRadio[i].checked = true;
+//   });
+// }
